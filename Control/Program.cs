9c8203b5ca0b6231a7ec235829109b447b1c1ed3
Evgenii <evgenii_08@list.ru>
2022-09-15 10:42:36 +0300

    AddedABlockDiagram

--- Control/Program.cs
+++ Control/Program.cs
@@ -3,28 +3,28 @@ Console.Write("Задайте количество строк: ");
 int length = Convert.ToInt32(Console.ReadLine());
 string[] array = new string [length];
 Console.WriteLine("Введите значение строки: ");
-int j = 0;
+int counter = 0;
 
 for (int i = 0; i < array.GetLength(0); i++)
 {
     array[i] = Convert.ToString(Console.ReadLine());
-    if(array[i].Length <= 3) j++;
+    if(array[i].Length <= 3) counter++;
 }
-string[] array2 = new string [j];
-j = 0;
+string[] secondArray = new string [counter];
+counter = 0;
 for (int i = 0; i < array.GetLength(0); i++)
 {
     if (array[i].Length <= 3)
     {
-        array2[j] = array[i];
-        j++;
+        secondArray[counter] = array[i];
+        counter++;
     }
 }
 PrintArray(array);
 Console.Write(" - ");
-PrintArray(array2);
+PrintArray(secondArray);
 
-void PrintArray(string [] array)  // Печатает массив
+void PrintArray(string [] array)          // Метод печатает массив
 {
     Console.Write("[");
     for (int i = 0; i < array.GetLength(0); i++)
